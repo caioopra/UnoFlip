a47df09bb8a11230ff11eee9f23f8d2c431b8325
fillipimangrich <costa.mangrich@gmail.com>
2023-04-02 22:46:36 -0300

    fix somes coordinates, add UNO button and add cheap

--- Table.py
+++ Table.py
@@ -25,9 +25,21 @@ class Table():
         background = self.canvas.create_image(
             0, 0,
             image=self.background_img,anchor="nw")
+
+        self.img0 = PhotoImage(file = f"table_images/ButtonUno.png")
+        button_start = self.canvas.create_image(640, 80, image=self.img0)
+        self.canvas.tag_bind(button_start, "<Button-1>", lambda x: print('Gritou uno'))
+        
+        card ='0_0'
+        image=Image.open(f'UNO_cards_small_light/{card}.png')
+        img=image.resize((100, 150))
+        self.dict_of_cards[card] = ImageTk.PhotoImage(img)
+        button_card = self.canvas.create_image(500, 300, image=self.dict_of_cards[card])
+        self.list_of_cards_in_hand.append((button_card,card))
+        self.canvas.tag_bind(button_card, "<Button-1>", lambda x:print('comprou carta'))
     
     def selectCard(self,card):
-        button_card = self.canvas.create_image(640, 360, image=self.dict_of_cards[card])
+        button_card = self.canvas.create_image(640, 300, image=self.dict_of_cards[card])
         self.canvas.tag_bind(button_card, "<Button-1>", lambda x:print('OI'))
         
         for k,i in enumerate(self.list_of_cards_in_hand):
@@ -57,6 +69,10 @@ class Table():
     def start(self):      
         self.setCanvas()
         self.createDesign()
+        
+
+
+
         self.addCard(card='1_6',x=340,y=570)
         self.addCard(card='2_6',x=460,y=570)
         self.addCard(card='3_6',x=580,y=570)
