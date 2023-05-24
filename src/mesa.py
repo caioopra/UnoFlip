45c39f1542a1c111df4b896428a9e8190b82ccb8
fillipimangrich <costa.mangrich@gmail.com>
2023-05-24 00:16:31 -0300

    fix

--- src/mesa.py
+++ src/mesa.py
@@ -1,14 +1,22 @@
-
+from random import randint
 
 
 class Mesa:
 
     def __init__(self,baralho) -> None:
         self.baralho = baralho
-        self.ultima_carta = self.baralho.cartas.pop()
+        self.setCartaInicial()
 
     def getUltimaCarta(self):
         return self.ultima_carta
     
     def setUltimaCarta(self, carta):
         self.ultima_carta=carta
+    
+    def setCartaInicial(self):
+        carta = self.baralho.cartas.pop()
+        self.setUltimaCarta(carta)
+        while carta.frente.tipo == 'coringa':
+            self.baralho.cartas.insert(randint(0, len(self.baralho.cartas)-1), carta)
+            carta = self.baralho.cartas.pop()
+            self.setUltimaCarta(carta)   
